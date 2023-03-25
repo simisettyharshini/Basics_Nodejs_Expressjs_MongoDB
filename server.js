a09d95a35e881f6a1b6f4e3d3f5ca2051167ee4d
simisettyharshini <simisettyharshini@gmail.com>
2023-03-25 13:33:27 +0530

    Updated files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8081; // local port num
 http
   .createServer((req, res) => { // call back func
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h2>hey server started n u can procced :-) 123456 </h2>");
+    res.write("<h2>hey server started n u can procced :-) 123456 oh.....:))</h2>");
     res.end();
   })
   .listen(port, () => { // call back func
