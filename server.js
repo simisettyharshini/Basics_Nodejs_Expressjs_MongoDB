fa5ad02894c7cfbf016cb6ce638c0ba5bb956ed8
simisettyharshini <123156760+simisettyharshini@users.noreply.github.com>
2023-03-25 21:37:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,12 +2,45 @@ const http = require("http");
 
 const port = 8081; // local port num
 
+//HTTP methods
+
+//GET MEthod : to get some data from server
+//POST Method : Sending data to server
+//DELETE :deleting the data from database
+//PATCH :updating certain feilds
+//PUT : full update 
+
+const toDolist = ["learn","apply","succed"];
+
+
 http
   .createServer((req, res) => { // call back func
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h2>hey server started n u can procced :-) 123456 oh.....:))</h2>");
+    const {method,url} = req
+   // console.log(method,url);
+   if(url === "/todos"){
+     if(method === "GET"){
+       res.writeHead(200,{"content-Type" : text/html});
+       res.write(toDoList.toString());
+
+     }
+     else{
+      res.writeHead(501);
+     }
+    }else{
+       res.writeHead(404);
+     } 
+     
+    
     res.end();
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    // res.write("<h2>hey server started n u can procced :-) 123456 oh.....:))</h2>");
+    // res.end();
   })
   .listen(port, () => { // call back func
     console.log(`NodeJs Server Started Running on Port: ${port}`);
   });
+//https://localhost:8081/signin
+//https://localhost:8081/signup
+//https://localhost:8081/aboutus
+//https://localhost:8081/contactus
+//https://localhost:8081/home
